c2f3e8423c5aaf72c6c81e082c9fc4f7130b5574
Shaazib Tanvir <syed2302995@st.jmi.ac.in>
2025-08-11 19:51:54 +0530

    feat: added rotate instructions

--- src/emu.rs
+++ src/emu.rs
@@ -1,3 +1,4 @@
+use crate::common::{OpCode, OpCodeError};
 use bitflags::bitflags;
 use std::cmp::Ordering;
 use std::collections::VecDeque;
@@ -335,286 +336,6 @@ pub struct CPU {
     cpu_state: CPUState,
     commands: VecDeque<Command>,
     instruction_count: u64,
-}
-
-macro_rules! convertible {
-    ($(#[$meta:meta])*
-     $vis:vis enum $name:ident {
-         $($field:ident $(= $value:literal)? $(,)?)*
-    }) => {
-        $(#[$meta])*
-        $vis enum $name {
-            $($field $(=  $value)?,)*
-        }
-
-        impl TryFrom<u8> for $name {
-            type Error = OpCodeError;
-            fn try_from(value: u8) -> Result<Self, OpCodeError> {
-                match value {
-                    $(x if x == $name::$field as u8 => Ok($name::$field),)*
-                    _ => Err(OpCodeError::Unused(value)),
-                }
-            }
-        }
-    }
-}
-
-convertible! {
-    #[derive(Debug)]
-    pub enum OpCode {
-        Nop = 0x00,
-        LxiB,
-        StaxB,
-        InxB,
-        InrB,
-        DcrB,
-        MviB,
-        Rlc,
-        DadB = 0x09,
-        LdaxB,
-        DcxB,
-        InrC,
-        DcrC,
-        MviC,
-        Rrc,
-        LxiD = 0x11,
-        StaxD,
-        InxD,
-        InrD,
-        DcrD,
-        MviD,
-        Ral,
-        DadD = 0x19,
-        LdaxD,
-        DcxD,
-        InrE,
-        DcrE,
-        MviE,
-        Rar,
-        Rim,
-        LxiH,
-        Shld,
-        InxH,
-        InrH,
-        DcrH,
-        MviH,
-        Daa,
-        DadH = 0x29,
-        Lhld,
-        DcxH,
-        InrL,
-        DcrL,
-        MviL,
-        Cma,
-        Sim,
-        LxiSp,
-        Sta,
-        InxSp,
-        InrM,
-        DcrM,
-        MviM,
-        Stc,
-        DadSP = 0x39,
-        Lda,
-        DcxSP,
-        InrA,
-        DcrA,
-        MviA,
-        Cmc,
-        MovBB,
-        MovBC,
-        MovBD,
-        MovBE,
-        MovBH,
-        MovBL,
-        MovBM,
-        MovBA,
-        MovCB,
-        MovCC,
-        MovCD,
-        MovCE,
-        MovCH,
-        MovCL,
-        MovCM,
-        MovCA,
-        MovDB,
-        MovDC,
-        MovDD,
-        MovDE,
-        MovDH,
-        MovDL,
-        MovDM,
-        MovDA,
-        MovEB,
-        MovEC,
-        MovED,
-        MovEE,
-        MovEH,
-        MovEL,
-        MovEM,
-        MovEA,
-        MovHB,
-        MovHC,
-        MovHD,
-        MovHE,
-        MovHH,
-        MovHL,
-        MovHM,
-        MovHA,
-        MovLB,
-        MovLC,
-        MovLD,
-        MovLE,
-        MovLH,
-        MovLL,
-        MovLM,
-        MovLA,
-        MovMB,
-        MovMC,
-        MovMD,
-        MovME,
-        MovMH,
-        MovML,
-        Hlt,
-        MovMA,
-        MovAB,
-        MovAC,
-        MovAD,
-        MovAE,
-        MovAH,
-        MovAL,
-        MovAM,
-        MovAA,
-        AddB,
-        AddC,
-        AddD,
-        AddE,
-        AddH,
-        AddL,
-        AddM,
-        AddA,
-        AdcB,
-        AdcC,
-        AdcD,
-        AdcE,
-        AdcH,
-        AdcL,
-        AdcM,
-        AdcA,
-        SubB,
-        SubC,
-        SubD,
-        SubE,
-        SubH,
-        SubL,
-        SubM,
-        SubA,
-        SbbB,
-        SbbC,
-        SbbD,
-        SbbE,
-        SbbH,
-        SbbL,
-        SbbM,
-        SbbA,
-        AnaB,
-        AnaC,
-        AnaD,
-        AnaE,
-        AnaH,
-        AnaL,
-        AnaM,
-        AnaA,
-        XraB,
-        XraC,
-        XraD,
-        XraE,
-        XraH,
-        XraL,
-        XraM,
-        XraA,
-        OraB,
-        OraC,
-        OraD,
-        OraE,
-        OraH,
-        OraL,
-        OraM,
-        OraA,
-        CmpB,
-        CmpC,
-        CmpD,
-        CmpE,
-        CmpH,
-        CmpL,
-        CmpM,
-        CmpA,
-        Rnz,
-        PopB,
-        Jnz,
-        Jmp,
-        Cnz,
-        PushB,
-        Adi,
-        Rst0,
-        Rz,
-        Ret,
-        Jz,
-        Cz = 0xCC,
-        Call,
-        Aci,
-        Rst1,
-        Rnc,
-        PopD,
-        Jnc,
-        Out,
-        Cnc,
-        PushD,
-        Sui,
-        Rst2,
-        Rc,
-        Jc = 0xDA,
-        In,
-        Cc,
-        Sbi = 0xDE,
-        Rst3,
-        Rpo,
-        PopH,
-        Jpo,
-        Xthl,
-        Cpo,
-        PushH,
-        Ani,
-        Rst4,
-        Rpe,
-        Pchl,
-        Jpe,
-        Xchg,
-        Cpe,
-        Xri = 0xEE,
-        Rst5,
-        Rp,
-        PopPSW,
-        Jp,
-        Di,
-        Cp,
-        PushPSW,
-        Ori,
-        Rst,
-        Rm,
-        Sphl,
-        Jm,
-        Ei,
-        Cm,
-        Cpi = 0xFE,
-        Rst7
-    }
-}
-
-#[derive(Error, Debug)]
-pub enum OpCodeError {
-    #[error("the opcode {0} is unused")]
-    Unused(u8),
 }
 
 const LIMIT: usize = 100;
@@ -1181,6 +902,98 @@ impl CPU {
                 let (sum1, carry1) = sum0.overflowing_add(old_carry);
 
                 self.push_add_op(sum1, Some(carry0 && carry1), Register::A);
+            }
+            OpCode::Ral => {
+                let shift_op = Operation::Register(RegisterOperation {
+                    old_value: self.cpu_state.get_register(Register::A),
+                    new_value: self.cpu_state.get_register(Register::A) << 1,
+                    register: Register::A,
+                });
+
+                let mut new_flags = self.cpu_state.get_flags().clone();
+                new_flags.set(
+                    Flags::CARRY,
+                    self.cpu_state.get_register(Register::A) & 0b10000000 != 0,
+                );
+                let flags_op = Operation::Flags(FlagsOperation {
+                    old_flags: self.cpu_state.get_flags(),
+                    new_flags: new_flags,
+                });
+
+                self.cpu_state.execute_op(shift_op);
+                self.cpu_state.execute_op(flags_op);
+                self.push_command(shift_op);
+                self.push_command(flags_op);
+            }
+            OpCode::Rar => {
+                let shift_op = Operation::Register(RegisterOperation {
+                    old_value: self.cpu_state.get_register(Register::A),
+                    new_value: self.cpu_state.get_register(Register::A) >> 1,
+                    register: Register::A,
+                });
+
+                let mut new_flags = self.cpu_state.get_flags().clone();
+                new_flags.set(
+                    Flags::CARRY,
+                    self.cpu_state.get_register(Register::A) & 0b00000001 != 0,
+                );
+                let flags_op = Operation::Flags(FlagsOperation {
+                    old_flags: self.cpu_state.get_flags(),
+                    new_flags: new_flags,
+                });
+
+                self.cpu_state.execute_op(shift_op);
+                self.cpu_state.execute_op(flags_op);
+                self.push_command(shift_op);
+                self.push_command(flags_op);
+            }
+            OpCode::Rlc => {
+                let shift_op = Operation::Register(RegisterOperation {
+                    old_value: self.cpu_state.get_register(Register::A),
+                    new_value: self.cpu_state.get_register(Register::A)
+                        << 1 + (self.cpu_state.get_register(Register::A) & 0b10000000 != 0) as u8,
+                    register: Register::A,
+                });
+
+                let mut new_flags = self.cpu_state.get_flags().clone();
+                new_flags.set(
+                    Flags::CARRY,
+                    self.cpu_state.get_register(Register::A) & 0b10000000 != 0,
+                );
+                let flags_op = Operation::Flags(FlagsOperation {
+                    old_flags: self.cpu_state.get_flags(),
+                    new_flags: new_flags,
+                });
+
+                self.cpu_state.execute_op(shift_op);
+                self.cpu_state.execute_op(flags_op);
+                self.push_command(shift_op);
+                self.push_command(flags_op);
+            }
+            OpCode::Rrc => {
+                let shift_op = Operation::Register(RegisterOperation {
+                    old_value: self.cpu_state.get_register(Register::A),
+                    new_value: self.cpu_state.get_register(Register::A)
+                        >> 1 + (((self.cpu_state.get_register(Register::A) & 0b00000001 != 0)
+                            as u8)
+                            << 7),
+                    register: Register::A,
+                });
+
+                let mut new_flags = self.cpu_state.get_flags().clone();
+                new_flags.set(
+                    Flags::CARRY,
+                    self.cpu_state.get_register(Register::A) & 0b10000000 != 0,
+                );
+                let flags_op = Operation::Flags(FlagsOperation {
+                    old_flags: self.cpu_state.get_flags(),
+                    new_flags: new_flags,
+                });
+
+                self.cpu_state.execute_op(shift_op);
+                self.cpu_state.execute_op(flags_op);
+                self.push_command(shift_op);
+                self.push_command(flags_op);
             }
             OpCode::InrA
             | OpCode::InrB
